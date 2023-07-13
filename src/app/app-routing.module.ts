e4257af21f4f6556835b63ec9dc7383439804ae1
MiguelSerki <miguel.serki@gmail.com>
2023-07-13 14:29:32 -0300

    3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '' //TODO: localhost:4200/
+    path: '', //TODO: localhost:4200/
+    loadChildren:() => import('./modules/home/home.module').then(m=> m.HomeModule)
   }
 ];
 
