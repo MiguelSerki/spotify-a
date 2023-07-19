621133b5e5908efa01f1aa9799a2819cbd645cfa
MiguelSerki <miguel.serki@gmail.com>
2023-07-19 16:14:58 -0300

    Auth

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthpageComponent } from './pages/authpage/authpage.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AuthpageComponent } from './pages/authpage/authpage.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/modules/auth/pages/authpage/authpage.component.ts
+++ src/app/modules/auth/pages/authpage/authpage.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-authpage',
   templateUrl: './authpage.component.html',
   styleUrls: ['./authpage.component.css']
 })
-export class AuthpageComponent {
+export class AuthpageComponent implements OnInit{
+  errorSession: boolean = false
+  formLogin: UntypedFormGroup = new UntypedFormGroup({});
+
+  constructor () {}
+
+  ngOnInit(): void {
+    this.formLogin = new UntypedFormGroup(
+      {
+        email: new UntypedFormControl('', [
+          Validators.required,
+          Validators.email
+        ]),
+        password: new UntypedFormControl('',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(12)
+          ])
+      }
+    )
+  }
+
+  sendLogin(): void {
+    const body = this.formLogin.value
+    console.log('KO', body);
+  }
 
 }
