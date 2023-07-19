92b3ee3c6c7d780dce16a5c8fd4749077f71cd57
MiguelSerki <miguel.serki@gmail.com>
2023-07-19 15:31:33 -0300

    img broken

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -9,11 +9,21 @@ import { TrackModel } from '@core/models/tracks.models';
 })
 export class PlayListBodyComponent implements OnInit{
 tracks: Array<TrackModel> = []
+optionSort: { property: string | null, order: string } = { property: null, order: 'asc' }
 
 constructor (){}
 
 ngOnInit(): void {
   
   this.tracks = dataRaw.data;
+}
+
+changeSort(property: string): void {
+  const { order } = this.optionSort
+  this.optionSort = {
+    property,
+    order: order === 'asc' ? 'desc' : 'asc'
+  }
+  console.log(this.optionSort);
 }
 }
